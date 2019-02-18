2045c5b303c6b29dd50f3d454470061e0e4a52b4
AnnZh <ann.zhurok@gmail.com>
2019-02-18 21:39:30 +0300

    Done

--- StringExtension.cs
+++ StringExtension.cs
@@ -6,7 +6,19 @@ namespace PadawansTask12
     {
         public static bool AllCharactersAreUnique(string source)
         {
-            throw new NotImplementedException();
+            if (source == null)
+                throw new ArgumentNullException();
+            //throw new NotImplementedException();
+            for (int i = 0; i < source.Length - 1; i++)
+            {
+                for(int j = i + 1; j < source.Length; j++)
+                {
+                    if (source[i] == source[j])
+                        return false;
+                }
+            }
+
+            return true;
         }
     }
 }
